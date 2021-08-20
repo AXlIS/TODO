cd30a7c6f1fe98a837b07f6d5b382a78aa32808d
Igor Svistun <igorekha2003@icloud.com>
2021-08-20 20:31:17 +0300

    start

--- library/library/settings.py
+++ library/library/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import json
 from pathlib import Path
+
+with open('secret_data/secret_key.json') as f:
+    data = json.load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p0u5@&v^6s%04t#+^t45ce_od)=8bf4w((!gy3yvh*67e@5wuj'
+SECRET_KEY = data.get('secretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3-d part apps
+    # customs apps
+    'authors',
 ]
 
 MIDDLEWARE = [
